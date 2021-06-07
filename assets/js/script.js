9c87e9801f107c99db9959028cf2fa75090ffd09
Jamie Kaczor <dignanjk@aol.com>
2021-06-06 20:03:11 -0400

    moar work

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const searchEl = document.getElementById("search-button");
+const clearEl = document.getElementById("clear-history");
+const cityEl = document.getElementById("enter-city");
+const nameEl = document.getElementById("city-name");
+const currentTempEl = document.getElementById("temperature");
+const currentHumidityEl = document.getElementById("humidity");
+const currentWindEl = document.getElementById("wind-speed");
+const currentUVEl = document.getElementById("UV-index");
+const currentPicEl = document.getElementById("current-pic");
+const historyEl = document.getElementById("history");
+var extendedEl = document.getElementById("extended");
+var todayweatherEl = document.getElementById("today-weather");
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
